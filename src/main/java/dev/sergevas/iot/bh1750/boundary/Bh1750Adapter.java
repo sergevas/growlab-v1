062ab7a17f9eb31082790e5d33c1d30851255b1f
Sergei Vasilev <sergei@localhost.localdomain>
2021-07-28 19:09:11 +0300

    In progress

--- src/main/java/dev/sergevas/iot/bh1750/boundary/Bh1750Adapter.java
+++ src/main/java/dev/sergevas/iot/bh1750/boundary/Bh1750Adapter.java
@@ -3,62 +3,45 @@ package dev.sergevas.iot.bh1750.boundary;
 import java.io.IOException;
 import java.util.Arrays;
 
-import com.pi4j.io.i2c.I2CBus;
-import com.pi4j.io.i2c.I2CDevice;
-import com.pi4j.io.i2c.I2CFactory;
-import com.pi4j.io.i2c.I2CFactory.UnsupportedBusNumberException;
-import com.pi4j.platform.PlatformAlreadyAssignedException;
+import com.pi4j.Pi4J;
+import com.pi4j.io.i2c.I2C;
+import com.pi4j.io.i2c.I2CProvider;
 import com.pi4j.util.Console;
-
-//import javax.enterprise.context.ApplicationScoped;
-//import javax.inject.Named;
+import com.pi4j.util.StringUtil;
 
 public class Bh1750Adapter {
 
-    public static final int GY_302_BH1750_ADDR = 0x23;
+    public static final int I2C_BUS = 1;
+    public static final int GY_302_BH1750_ADDR = 0x23; // Default address for the GY-302 BH1750 chip
     public static final byte GY_302_BH1750_POWER_DOWN = 0x00;
     public static final byte GY_302_BH1750_POWER_ON = 0x01;
     public static final byte GY_302_BH1750_ONE_TIME_H_RESOLUTION_MODE_2 = (byte) 0x21;
+    public static final int GY_302_BH1750_READINGS_DATA_LENGTH = 2;
 
-    public static void main(String[] args) throws InterruptedException, IOException, UnsupportedBusNumberException {
+    public static void main(String[] args) {
 
         final Console console = new Console();
         console.promptForExit();
-        try {
-            int[] ids = I2CFactory.getBusIds();
-            console.println("Found follow I2C busses: " + Arrays.toString(ids));
-        } catch (IOException exception) {
-            console.println("I/O error during fetch of I2C busses occurred");
+
+        var pi4j = Pi4J.newAutoContext();
+        var config = I2C.newConfigBuilder(pi4j)
+                .id("growlab-i2c-bus")
+                .bus(I2C_BUS)
+                .device(GY_302_BH1750_ADDR)
+                .build();
+        I2CProvider i2CProvider = pi4j.provider("pigpio-i2c");
+        try (var i2cDevice = i2CProvider.create(config)) {
+            i2cDevice.write(GY_302_BH1750_POWER_ON);
+            Thread.sleep(1);
+            i2cDevice.write(GY_302_BH1750_ONE_TIME_H_RESOLUTION_MODE_2);
+            Thread.sleep(200);
+            console.println("Reading data from GY-302 BH1750...");
+            byte[] readings = i2cDevice.readNBytes(GY_302_BH1750_READINGS_DATA_LENGTH);
+            console.println("GY-302 BH1750 readings: " + StringUtil.toHexString(readings));
+            i2cDevice.write(GY_302_BH1750_POWER_DOWN);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-
-        for (int number = I2CBus.BUS_0; number <= I2CBus.BUS_17; ++number) {
-            try {
-                @SuppressWarnings("unused")
-                I2CBus bus = I2CFactory.getInstance(number);
-                console.println("Supported I2C bus " + number + " found");
-            } catch (IOException exception) {
-                console.println("I/O error on I2C bus " + number + " occurred");
-            } catch (UnsupportedBusNumberException exception) {
-                console.println("Unsupported I2C bus " + number + " required");
-            }
-        }
-
-        I2CBus i2c = I2CFactory.getInstance(I2CBus.BUS_1);
-
-        // create an I2C device for an individual device on the bus that you want to
-        // communicate with
-        // in this example we will use the default address for the GY-302 BH1750 chip
-        // which is 0x23.
-        I2CDevice device = i2c.getDevice(GY_302_BH1750_ADDR);
-        device.write(GY_302_BH1750_POWER_ON);
-        Thread.sleep(1);
-        device.write(GY_302_BH1750_ONE_TIME_H_RESOLUTION_MODE_2);
-        Thread.sleep(200);
-        console.println("... reading DATA registers from GY-302 BH1750");
-        byte[] readings = new byte[2];
-        int bytesRead = device.read(readings, 0, 2);
-        console.println("GY-302 BH1750 bytes read: " + bytesRead);
-        console.println("GY-302 BH1750 readings: " + Arrays.toString(readings));
-        // device.write(GY_302_BH1750_POWER_DOWN);
+        pi4j.shutdown();
     }
 }
